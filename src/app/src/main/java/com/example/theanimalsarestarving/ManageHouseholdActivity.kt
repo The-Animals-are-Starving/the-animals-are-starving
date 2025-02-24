47589187efe40aa2aef44d1ad4c06bba86a57af0
Aidan Cotsakis <aidancotsakis@gmail.com>
2025-02-24 02:09:35 -0800

    Made user list and pet list expand with more users/pets, made add pet button work

--- src/app/src/main/java/com/example/theanimalsarestarving/ManageHouseholdActivity.kt
+++ src/app/src/main/java/com/example/theanimalsarestarving/ManageHouseholdActivity.kt
@@ -22,8 +22,14 @@ class ManageHouseholdActivity : AppCompatActivity() {
         val userListContainer = findViewById<LinearLayout>(R.id.userListContainer)
         val newUserButton = findViewById<Button>(R.id.newUserButton)
 
+        val petListContainer = findViewById<LinearLayout>(R.id.petListContainer)
+        val newPetButton = findViewById<Button>(R.id.newPetButton)
+
         newUserButton.setOnClickListener {
             showAddUserDialog(userListContainer)
+        }
+        newPetButton.setOnClickListener {
+            showAddPetDialog(petListContainer)
         }
     }
 
@@ -75,6 +81,59 @@ class ManageHouseholdActivity : AppCompatActivity() {
 
     private fun userExists(name: String, container: LinearLayout): Boolean {
         //TODO: Check user list to see if user already exists
+        return false
+    }
+
+
+    private fun showAddPetDialog(container: LinearLayout) { //Popup for new pet
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Add New Pet")
+
+        val dialog = builder.create()
+
+        val input = EditText(this).apply {
+            hint = "Enter new pet"
+            setSingleLine(true)
+            imeOptions = EditorInfo.IME_ACTION_DONE
+        }
+        builder.setView(input)
+
+        // Submit
+        builder.setPositiveButton("Add") { _, _ ->
+            val petName = input.text.toString().trim().replace("\n", "")
+            if (petName.isNotEmpty()) {
+                addPet(petName, container)
+            }
+        }
+
+        // Set negative button (cancel)
+        builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+
+
+        builder.show()
+    }
+
+    private fun addPet(name: String, container: LinearLayout) {
+        if (!petExists(name, container)) {
+            //TODO: modify this for the needs of a pet
+            val switch = SwitchCompat(this).apply {
+                text = name
+                isChecked = false
+            }
+            container.addView(switch)
+
+            //TODO: Also send pet to backend
+        } else {
+            AlertDialog.Builder(this)
+                .setMessage("Pet Already Exists!")
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+        }
+    }
+
+    private fun petExists(name: String, container: LinearLayout): Boolean {
+        //TODO: Check pet list to see if pet already exists
         return false
     }
 }
