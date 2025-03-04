cf643cd8483495b41c969aee39f2a296046c0eee
matthewfung04 <matthewfung9001@gmail.com>
2025-03-03 20:54:53 -0800

    pausing backend firebase

--- Backend/config/firebase.ts
+++ Backend/config/firebase.ts
@@ -1,15 +1,15 @@
-import admin, { ServiceAccount } from "firebase-admin";
-import { initializeApp, cert } from "firebase-admin/app";
-const serviceAccountKeyStr = process.env.FIREBASE_SERVICE_ACCOUNT;
+// import admin, { ServiceAccount } from "firebase-admin";
+// import { initializeApp, cert } from "firebase-admin/app";
+// const serviceAccountKeyStr = process.env.FIREBASE_SERVICE_ACCOUNT;
 
-if (!serviceAccountKeyStr) {
-    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
-}
+// if (!serviceAccountKeyStr) {
+//     throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
+// }
 
-const serviceAccountKey: ServiceAccount = JSON.parse(serviceAccountKeyStr);
+// const serviceAccountKey: ServiceAccount = JSON.parse(serviceAccountKeyStr);
 
-initializeApp({
-    credential: cert(serviceAccountKey),
-});
+// initializeApp({
+//     credential: cert(serviceAccountKey),
+// });
 
-export default admin;
+// export default admin;
