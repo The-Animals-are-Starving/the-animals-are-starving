5318a3508ebd9db9087af24fcae78b1decb82340
matthewfung04 <matthewfung9001@gmail.com>
2025-03-03 20:48:00 -0800

    fixing leaked key

--- Backend/config/firebase.ts
+++ Backend/config/firebase.ts
@@ -1,15 +1,15 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "../config/the-animals-are-starving-firebase-adminsdk-fbsvc-e38aec3b3e.json";
+const serviceAccountKeyStr = process.env.FIREBASE_SERVICE_ACCOUNT;
 
-const serviceAccountKey: ServiceAccount = {
-  projectId: serviceAccount.project_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-};
+if (!serviceAccountKeyStr) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
+}
+
+const serviceAccountKey: ServiceAccount = JSON.parse(serviceAccountKeyStr);
 
 initializeApp({
-  credential: cert(serviceAccountKey),
+    credential: cert(serviceAccountKey),
 });
 
 export default admin;
