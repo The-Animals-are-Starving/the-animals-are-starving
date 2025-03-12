807e15004b7104e8fb1954a2d0d866d1787fabb4
matthewfung04 <matthewfung9001@gmail.com>
2025-03-12 14:52:20 -0700

    jest test

---
+++ Backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverageFrom: [
+      "controllers/**/*.ts",
+      "routes/**/*.ts",
+      "models/**/*.ts"
+    ],
+  };
+  
